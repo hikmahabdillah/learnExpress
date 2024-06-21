92797ee8a27002b25f92748cd1161c6e4df3ec81
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-06-21 18:17:01 +0700

    try ejs as template engine(like jsx, tsx, ntl, etc..)

--- app.js
+++ app.js
@@ -49,17 +49,18 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+// USE EJS
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.sendFile("./index.html", { root: __dirname });
-  // res.send("Hello World!");
+  res.render("index");
 });
 app.get("/about", (req, res) => {
-  res.sendFile("./about.html", { root: __dirname });
-  // res.send("this is About Page");
+  res.render("about");
 });
 app.get("/contact", (req, res) => {
-  res.sendFile("./contact.html", { root: __dirname });
-  // res.send("this is contact Pagess");
+  res.render("contact");
 });
 
 // for request anything
